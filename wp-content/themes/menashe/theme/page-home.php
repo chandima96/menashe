febea0810930d729ebe1c85872a60daa0cc456cf
Nikalanda Bandara <chandima96dilan@gmail.com>
2024-07-01 09:58:30 +0530

    Test

--- wp-content/themes/menashe/theme/page-home.php
+++ wp-content/themes/menashe/theme/page-home.php
@@ -88,8 +88,16 @@
                         <img src="<?php echo get_template_directory_uri(); ?>/images/Untitled-2.jpg" alt="Image 2" class="carousel-image">
                         <img src="<?php echo get_template_directory_uri(); ?>/images/Untitled-2.jpg" alt="Image 3" class="carousel-image">
                     </div>
-                    <button class="carousel-button prev" id="prevBtn">‹</button>
-                    <button class="carousel-button next" id="nextBtn">›</button>
+                    <button class="carousel-button prev" id="prevBtn">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
+                        </svg>
+                    </button>
+                    <button class="carousel-button next" id="nextBtn">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>
+                        </svg>
+                    </button>
                 </div>
             </div>
 
