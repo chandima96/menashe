5fe9345501c7ea239260c67f09f60b20b1cd803b
Nikalanda Bandara <chandima96dilan@gmail.com>
2024-07-01 11:17:08 +0530

    Test

--- wp-content/themes/menashe/theme/page-home.php
+++ wp-content/themes/menashe/theme/page-home.php
@@ -92,7 +92,10 @@
                             <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>
                         </svg>
                     </button>
-                    <div class="absolute left-0 top-0 lg:bottom-0 lg:right-0 bg-white shadow-lg">
+                    <div class="absolute hidden lg:block bottom-0 right-0 bg-white shadow-lg">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/logo-2.png" class="p-2 w-full h-14" alt="">
+                    </div>
+                    <div class="absolute block lg:hidden left-0 top-0 bg-white shadow-lg">
                         <img src="<?php echo get_template_directory_uri(); ?>/images/logo-2.png" class="p-2 w-full h-14" alt="">
                     </div>
                 </div>
