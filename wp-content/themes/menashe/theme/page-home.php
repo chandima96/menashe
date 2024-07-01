9ee1b34a345f788e2858ad712c9f23772724ab61
Nikalanda Bandara <chandima96dilan@gmail.com>
2024-07-01 18:08:39 +0530

    Test

--- wp-content/themes/menashe/theme/page-home.php
+++ wp-content/themes/menashe/theme/page-home.php
@@ -124,7 +124,13 @@
                         </div>
                         <div class="flex gap-3 lg:block">
                             <div class="">
-                                <button id="submit" type="submit" class="w-full bg-submit h-10 text-black mt-2">שליחה</button>
+                                <button id="submit" type="submit" class="lg:w-full w-32 rounded-full bg-submit h-10 text-black mt-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-chevron-double-left" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
+                                    <path fill-rule="evenodd" d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
+                                </svg>
+                                שליחה
+                                </button>
                             </div>
                             <div class="">
                                 <div class="flex">
@@ -145,30 +151,7 @@
     </div>
 </div>
 
-<div class="lg:hidden block w-full fixed bottom-0 bg-white">
-    <div class="grid grid-cols-2 gap-3 m-3">
-        <div>
-            <button class="flex justify-center items-center border-2 border-black bg-[#2ed02e] w-full text-white p-1">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-telephone-inbound-fill" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877zM15.854.146a.5.5 0 0 1 0 .708L11.707 5H14.5a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 1 0v2.793L15.146.146a.5.5 0 0 1 .708 0"/>
-                </svg>
-                <div class="text-3xl md:text-4xl Karantina pl-3">לשיחה עם נציג</div>
-            </button>
-        </div>
-        <div>
-            <button class="flex justify-center items-center border-2 border-black bg-submit w-full text-black p-1">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-chevron-double-left" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
-                    <path fill-rule="evenodd" d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
-                </svg>
-                <div class="text-3xl md:text-4xl Karantina pl-3">לפרטים נוספים</div>
-            </button>
-        </div>
-    </div>
 
-
-
-</div>
 
 <script>
     document.addEventListener('DOMContentLoaded', function() {
