4beba44a56f9bb3e8ef731ee7e54b2b202ae7ecf
Nikalanda Bandara <chandima96dilan@gmail.com>
2024-07-01 11:04:03 +0530

    Test

--- wp-content/themes/menashe/theme/page-home.php
+++ wp-content/themes/menashe/theme/page-home.php
@@ -92,6 +92,9 @@
                             <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>
                         </svg>
                     </button>
+                    <div class="absolute bottom-4 right-4 bg-white p-4 rounded-lg shadow-lg">
+                        Your content here
+                    </div>
                 </div>
             </div>
 
