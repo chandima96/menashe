24cc47cfde553d0ba2df54a704d62e2059bfa18e
Nikalanda Bandara <chandima96dilan@gmail.com>
2024-07-01 17:00:03 +0530

    Test

--- wp-content/themes/menashe/theme/page-home.php
+++ wp-content/themes/menashe/theme/page-home.php
@@ -47,20 +47,20 @@
                 <div class="flex justify-center w-full">
                     <img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" class="h-auto w-28 flex justify-center" alt="">
                 </div>
-                <h1 class="text-center font-bold md:text-5xl font-heebo"><?php echo get_field('home_title'); ?></h1>
+                <h1 class="text-center font-bold text-4xl md:text-5xl font-heebo"><?php echo get_field('home_title'); ?></h1>
 
                 <div class="text-center mx-3 mt-6">
                     <div class="bg-submit h-20 w-full flex items-center justify-center">
-                        <p class="text-white font-extrabold text-3xl md:text-5xl leading-[48px] font-heebo"><?php echo get_field('gold_box_text'); ?></p>
+                        <p class="text-white font-extrabold text-4xl md:text-5xl leading-[48px] font-heebo"><?php echo get_field('gold_box_text'); ?></p>
                         <img src="<?php echo get_template_directory_uri(); ?>/images/1585.png" class="" alt="">
                     </div>
-                    <div class="bg-black text-white h-8 w-full font-bold md:text-2xl">
+                    <div class="bg-black text-white h-8 w-full font-bold font-heebo md:text-2xl">
                         <?php echo get_field('black_box_text'); ?>
                     </div>
                 </div>
                 <div class="text-[#c8a950] w-full text-center flex justify-center mt-6">
-                    <p class="font-bold md:text-3xl mt-auto font-heebo"><?php echo get_field('gold_text_one'); ?></p>
-                    <p class="font-bold md:text-6xl font-heebo"><?php echo get_field('gold_text_two'); ?> </p>
+                    <p class="font-bold text-2xl md:text-3xl mt-auto font-heebo"><?php echo get_field('gold_text_one'); ?></p>
+                    <p class="font-bold text-5xl md:text-6xl font-heebo"><?php echo get_field('gold_text_two'); ?> </p>
                     <p class="text-3xl font-semibold my-auto font-heebo"><?php echo get_field('gold_text_three'); ?></p>
                 </div>
                 <div class="text-[#276658] w-full font-bold md:text-2xl text-center mt-6 font-heebo">
@@ -147,7 +147,7 @@
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-telephone-inbound-fill" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877zM15.854.146a.5.5 0 0 1 0 .708L11.707 5H14.5a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 1 0v2.793L15.146.146a.5.5 0 0 1 .708 0"/>
                 </svg>
-                <div class="md:text-4xl font-karantina pl-3">לשיחה עם נציג</div>
+                <div class="text-3xl md:text-4xl font-karantina pl-3">לשיחה עם נציג</div>
             </button>
         </div>
         <div>
@@ -156,7 +156,7 @@
                     <path fill-rule="evenodd" d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
                     <path fill-rule="evenodd" d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
                 </svg>
-                <div class="md:text-4xl font-karantina pl-3">לפרטים נוספים</div>
+                <div class="text-3xl md:text-4xl font-karantina pl-3">לפרטים נוספים</div>
             </button>
         </div>
     </div>
