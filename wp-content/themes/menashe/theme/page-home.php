6e1828b0a893eb2a5e0de5a4e55d61275ac3111d
Karu97 <karunanayake234@gmail.com>
2024-07-02 18:17:36 +0530

    Test

--- wp-content/themes/menashe/theme/page-home.php
+++ wp-content/themes/menashe/theme/page-home.php
@@ -150,6 +150,11 @@ display:none;
     gap:6px;
 }
 
+@media (min-width: 1024px) and (max-width: 1279px) {
+    .custom-query {
+        font-size: 28px !important;
+    }
+}
 </style>
 
 <div class="mb-[72px] lg:mb-0">
@@ -160,7 +165,7 @@ display:none;
                     <div class="flex justify-center w-full">
                         <img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" class="h-auto w-24 2xl:w-44 xl:w-32 flex justify-center" alt="">
                     </div>
-                    <h1 class="text-center font-bold text-black text-4xl lg:text-[2.25rem] xl:text-4xl 2xl:text-[3.25rem] Heebo"><?php echo get_field('home_title'); ?></h1>
+                    <h1 class="text-center font-bold text-black text-4xl lg:text-[2.25rem] xl:text-4xl 2xl:text-[3.25rem] Heebo custom-query"><?php echo get_field('home_title'); ?></h1>
 
                     <div class="text-center mx-3">
                         <div class="bg-submit h-16 w-full flex items-center justify-center">
