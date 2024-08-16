717975ba70abdacf5e43d0b7f84236e064640eea
Karu97 <karunanayake234@gmail.com>
2024-08-16 19:42:28 +0530

    Test

--- wp-content/themes/assignment/theme/page-home.php
+++ wp-content/themes/assignment/theme/page-home.php
@@ -130,6 +130,11 @@ get_header();
         <img src="<?php echo get_template_directory_uri(); ?>/img/Female Team Member.png" class="w-40" alt="">
         <h4 class="text-lg font-bold text-[#3D3D3D]">Team Member Name</h4>
         <p class="text-[#3D3D3D] font-normal">Team Member Designation</p>
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="text-gray-500 hover:text-[#A4B61D] transition-colors duration-300 ease-in-out" viewBox="0 0 16 16">
+            <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
+          </svg>
+        </div>
       </div>
       
   
